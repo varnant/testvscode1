c8cd88c2d7974081d53e705485838ef589ed7d5c
varnant <varnant@gmail.com>
2025-12-09 09:55:14 +0530

    test 1

---
+++ one.java
@@ -0,0 +1,15 @@
+class one{
+
+    public static void main(String[] args) {
+        System.out.println("hello vs code");
+    new one().addTwonunmber(5, 10);
+   
+    new one().subTwoNumber(10, 5);
+    }
+    public void addTwonunmber(int a , int b){
+        System.out.println(a+b);
+    }
+    public void subTwoNumber(int a , int b){
+        System.out.println(a-b);
+    }
+}
