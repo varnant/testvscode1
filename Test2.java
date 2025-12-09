ef1b2a737fb3c8cbf65d2816bfa6cc5351d5d2a4
varnant <varnant@gmail.com>
2025-12-09 10:13:18 +0530

    test2 added

---
+++ Test2.java
@@ -0,0 +1,6 @@
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! git hub copilot helped");
+    }
+    
+}
--- one.java
+++ one.java
@@ -5,11 +5,15 @@ class one{
     new one().addTwonunmber(5, 10);
    
     new one().subTwoNumber(10, 5);
+    new one().multiplyTwoNumber(5, 10);
     }
     public void addTwonunmber(int a , int b){
         System.out.println(a+b);
     }
     public void subTwoNumber(int a , int b){
         System.out.println(a-b);
+    }
+    public void multiplyTwoNumber(int a, int b){
+        System.out.println(a*b);
     }
 }
